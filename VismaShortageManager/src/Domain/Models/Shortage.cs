1d5f25e1affa53bf3bb8714b2af30451bf402cc5
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2024-06-28 12:40:55 +0300

    Update Shortage model

--- VismaShortageManager/src/Domain/Models/Shortage.cs
+++ VismaShortageManager/src/Domain/Models/Shortage.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using VismaShortageManager.src.Domain.Enums;
 
-public class Shortage
+namespace VismaShortageManager.src.Domain.Models
 {
-    public string Title { get; set; }
-    public string Name { get; set; }
-    public RoomType Room { get; set; }
-    public CategoryType Category { get; set; }
-    public int Priority { get; set; }
-    public DateTime CreatedOn { get; set; }
+    public class Shortage
+    {
+        public required string Title { get; set; }  // Title of shortage request
+        public RoomType Room { get; set; }  // Room where shortage appear
+        public CategoryType Category { get; set; }  // Category of shortage request
+        public int Priority { get; set; }   // Priority of request
+        public DateTime CreatedOn { get; set; } // Track the date of request creation
+        public required string CreatedBy { get; set; }  // Track the creator of request
+    }
 }
