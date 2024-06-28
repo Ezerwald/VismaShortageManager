427af8263e3500ddb5b118eb9f456a4a27cbeae5
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2024-06-28 12:41:07 +0300

    Create user model

---
+++ VismaShortageManager/src/Domain/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VismaShortageManager.src.Domain.Models
+{
+    public class User
+    {
+        public required string Name { get; set; }
+        public bool IsAdministrator { get; set; }
+    }
+}
